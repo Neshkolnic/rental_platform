7ffb2bdd1d4e71f902aaec451c24a92502753929
David Lukin <david.lukin.99@bk.ru>
2025-04-03 14:38:26 +0300

    Обновли регистрацию

--- booking/views.py
+++ booking/views.py
@@ -2,17 +2,26 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import UserRegisterForm, UserLoginForm
 
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = UserRegisterForm()
+#     return render(request, 'registration/register.html', {'form': form})
 def register_view(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
+            # Указываем backend явно
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
             return redirect('home')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'registration/register.html', {'form': form})
-
+        else:
+            return render(request, 'registration/register.html', {'form': form})
 def login_view(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
--- core/settings.py
+++ core/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
