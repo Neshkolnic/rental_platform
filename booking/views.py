6939a73e0fc9f448653ff1a23d05058a5ee6cc28
Neshkolnic <rgshfvag@gmail.com>
2025-04-04 21:45:44 +0300

    добавил объявления(не работает добавление из-за траблов с коордиантами)

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth.decorators import login_required
+from .forms import PropertyForm
+from .models import Property
+from django.http import JsonResponse
+from geopy.geocoders import Nominatim
+
+
+def geocode_view(request):
+    address = request.GET.get('address', '')
+    geolocator = Nominatim(user_agent="property_app")
+    try:
+        location = geolocator.geocode(address)
+        if location:
+            return JsonResponse({'lat': location.latitude, 'lon': location.longitude})
+    except:
+        pass
+    return JsonResponse({'error': 'Адрес не найден'}, status=400)
 
 # def register_view(request):
 #     if request.method == 'POST':
@@ -36,3 +53,28 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+@login_required
+def create_property(request):
+    if request.method == 'POST':
+        form = PropertyForm(request.POST)
+        if form.is_valid():
+            property = form.save(commit=False)
+            property.owner = request.user
+            property.save()
+            return redirect('property_list')  # Или на страницу успеха
+    else:
+        form = PropertyForm()
+
+    return render(request, 'property/create.html', {'form': form})
+
+# views.py
+def property_list(request):
+    properties = Property.objects.filter(is_active=True)
+    return render(request, 'property/list.html', {'properties': properties})
+
+@login_required
+def my_properties(request):
+    properties = Property.objects.filter(owner=request.user)
+    return render(request, 'property/my_properties.html', {'properties': properties})
